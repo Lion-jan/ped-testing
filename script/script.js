b19d4fcc822f4f774f3bc5096cb5aaf42f45d1b7
Lion-jan <islomakapardayev75@gmail.com>
2023-11-19 22:03:13 +0500

    git ga yuklandi

---
+++ script/script.js
@@ -0,0 +1,362 @@
+let savollar = [
+    {
+        savol: 'O‘zbekiston respublikasining “ta’lim to‘g‘risida”gi qonuniga muvofiq kimlar pedagogik faoliyat bilan shug‘ullanish huquqiga egalar?',
+        variantlar: ["..tegishli ma’lumoti, kasbiy tayyorgarligi bor va yuksak axloqiy fazilatlarga ega bo‘lgan shaxslar",
+            "kasbiy bilim, ko‘nikma va malaka, shuningdek, ta’lim muassasalarida ishlash tajribasiga ea shaxslar",
+            "oliy pedagogik ma’lumot hamda 5 yillik pedagogik stajga ega shaxslar",
+            "faoliyat jarayonida ta’lim oluvchilarning yosh va psixologik xususiyatlarini inobatga olgan holda ular bilan muloqot qilish malakasini o‘zlashtirgan shaxslar"],
+        javob: '..tegishli ma’lumoti, kasbiy tayyorgarligi bor va yuksak axloqiy fazilatlarga ega bo‘lgan shaxslar'
+    },
+
+    {
+        savol: 'ta’lim muassasasi nima?',
+        variantlar: ["..qonun hujjatlariga muvofiq ishlab chiqilgan ustav asosida faoliyat ko‘rsatadigan yuridik shaxs",
+            'pulli ta’lim xizmatlarini ko‘rsatish orqali moliyaviy imkoniyatlarini kengaytirish huquqiga ega yuridik shaxs',
+            ' rivojlangan xorijiy mamlamatlarning ta’lim muassasalari bilan hamkorlikni yo‘lga qo‘yish huquqiga ega yuridik shaxs',
+            ' ta’lim sohasidagi islohotlarni tashkil etishga mas’ul yuridik shaxs'],
+        javob: '..qonun hujjatlariga muvofiq ishlab chiqilgan ustav asosida faoliyat ko‘rsatadigan yuridik shaxs'
+    },
+
+    {
+        savol: 'mashg’ulotlarga qo’yiladigan maqsadlar?',
+        variantlar: ['..ta’limiy, tarbiyaviy, rivojlantiruvchi',
+            ' tarbiyaviy, didaktik, ijtimoiy-g‘oyaviy',
+            'tarbiyaviy, didaktik, muloqot',
+            ' ijtimoiy-g‘oyaviy, aksiologik, didaktik'],
+        javob: '..ta’limiy, tarbiyaviy, rivojlantiruvchi'
+    },
+
+    {
+        savol: 'ta’lim to’g’risidagi qonun” qachon qabul qilingan?',
+        variantlar: ['..1997 yil, 29 avgust',
+            '1997 yil, 29 iyul',
+            '1992 yil, 12 avgust',
+            '1993 yil, 11 avgust'],
+        javob: '..1997 yil, 29 avgust'
+    },
+
+    {
+        savol: 'kadrlar tayyorlash milliy modelining tarkibiy qismlari qaysi variantda berilgan?',
+        variantlar: ['..shaxs, uzluksiz ta’lim, davlat va jamiyat, fan, ishlab chiqarish', ' ta’lim xizmatlarini ko‘rsatuvchilar, ishlab chiqarish',
+            ' ta’lim xizmatlarining iste’molchilari, ishlab chiqarish',
+            'ta’lim jarayonining ishtirokchilari, ishlab chiqarish'],
+        javob: '..shaxs, uzluksiz ta’lim, davlat va jamiyat, fan, ishlab chiqarish'
+    },
+
+    {
+        savol: 'davlat ta’lim standartlari nima?',
+        variantlar: ['..umumiy o‘rta, o‘rta maxsus, kasb-hunar va oliy ta’lim mazmuniga hamda sifatiga qo‘yiladigan talablarni belgilaydigan me’yoriy hujjat',
+            ' o‘quv yuklamlari hajmini aniqlashga xizmat qiladigan, shakl va metodlari to‘g‘risida mezonlar yig‘indisi',
+            ' ta’lim jarayonining samarali tashkil etish yo‘llari, shakl va metodlari to‘g‘risida ma’lumotlardan iborat yo‘riqnoma', 'o‘quv jadvalining tarkibiy elementlarini aks ettiruvchi manba, shakl va metodlari to‘g‘risida'],
+        javob: '..umumiy o‘rta, o‘rta maxsus, kasb-hunar va oliy ta’lim mazmuniga hamda sifatiga qo‘yiladigan talablarni belgilaydigan me’yoriy hujjat'
+    },
+
+    {
+        savol: ' DTS qanday ta’lim turlari uchun joriy etiladi? ',
+        variantlar: ['..umumiy o‘rta ta’lim, oliy ta’lim',
+            'umumiy o‘rta, maktabdan tashqari ta’lim, oliy ta’lim',
+            'maktabgacha, umumiy o‘rta, o‘rta maxsus, kasb-xunar',
+            ' bolalar bog‘chasi, umumiy o‘rta ta’lim, o‘rta maxsus'],
+        javob: '..umumiy o‘rta ta’lim, oliy ta’lim'
+    },
+
+    {
+        savol: 'pedagogika” tushunchasi qanday ma’noni anglatadi?',
+        variantlar: ['..bola yetaklayman',
+            'shaxsni tarbiyalayman',
+            'shaxsni rivojlantiraman',
+            'bolani voyaga etkazaman'],
+        javob: '..bola yetaklayman'
+    },
+
+    {
+        savol: 'pedagogika bola tarbiyasining fani demakdir”. Ta’rif kim tomonidan berilgan?',
+        variantlar: ['..Abdulla Avloniy',
+            ' H.H.Aiyoziy',
+            'Abduqodir Shakuriy',
+            'Is`hoqxon Ibrat'],
+        javob: '..Abdulla Avloniy'
+    },
+
+    {
+        savol: ' pedagogika fani konsepsiyasida nimalar aks etadi?',
+        variantlar: ['..fanning rivojlanish istiqbollari',
+            'fanning umumiy mazmuni',
+            'fan erishgan yutuqlar',
+            'fanning yangi yo‘nalishlari'],
+        javob: '..fanning rivojlanish istiqbollari'
+    },
+
+    {
+        savol: ' pedagogika fanining muhim tarkibiy qismlari qaysi javobda to‘g‘ri ko‘rsatilgan?',
+        variantlar: ['..didaktika (ta’lim nazariyasi), tarbiya nazariyasi',
+            ' ta’lim menejmenti, didaktika (ta’lim nazariyasi)',
+            'didaktika (ta’lim nazariyasi), maktabshunoslik',
+            'tarbiya nazariyasi, ta’lim menejmenti'],
+        javob: '..didaktika (ta’lim nazariyasi), tarbiya nazariyasi'
+    },
+
+    {
+        savol: ' pedagogika fanlari tizimi qanday fanlardan tarkib topgan?',
+        variantlar: ['..maktabgacha ta’lim pedagogikasi”, “boshlang‘ich ta’lim pedagogikasi”, “pedagogika nazariyasi”, “pedagogika tarixi”, “pedagogik mahorat va pedagogik texnologiya”, “ijtimoiy pedagogika”, “kasbiy pedagogika”, “maxsus pedagogika”, “ta’lim menejmenti”, “qiyosiy pedagogika',
+            'ijtimoiy pedagogika”, “kasbiy pedagogika”, “maxsus pedagogika',
+            'pedagogika nazariyasi”, “pedagogika tarixi”, “pedagogik mahorat”, “maxsus pedagogika”, “ta’lim menejmenti”, “qiyosiy pedagogika',
+            'pedagogika nazariyasi”, “pedagogika tarixi”, “pedagogik texnologiya'],
+        javob: '..maktabgacha ta’lim pedagogikasi”, “boshlang‘ich ta’lim pedagogikasi”, “pedagogika nazariyasi”, “pedagogika tarixi”, “pedagogik mahorat va pedagogik texnologiya”, “ijtimoiy pedagogika”, “kasbiy pedagogika”, “maxsus pedagogika”, “ta’lim menejmenti”, “qiyosiy pedagogika'
+    },
+
+    {
+        savol: 'pedagogikaning qaysi sohasi shaxsiy va faoliyatida nuqsonlar ko‘zga tashlanadigan bolalarni o‘qitish va tarbiyalash bilan shug‘ullanadi?',
+        variantlar: ['..maxsus pedagogika', 'kasbiy pedagogika', 'ijtimoiy pedagogika', 'qiyosiy pedagogika'],
+        javob: '..maxsus pedagogika'
+    },
+
+    {
+        savol: 'pedagogikaning asosiy kategoriyalari hisoblanadi?',
+        variantlar: ['..tarbiya, o‘qitish, bilim olish (ma’lumot), rivojlanish',
+            'tarbiya, ta’lim, ma’lumot, shakllantirish',
+            ' ta’lim, ma’lumot, shaxs, muloqot',
+            'ma’lumot, shaxs, ta’lim, rivojlantirish'],
+        javob: '..tarbiya, o‘qitish, bilim olish (ma’lumot), rivojlanish'
+    },
+    {
+        savol: 'ta’limning mazmuni qanday manbalarda aks ettiriladi?',
+        variantlar: ['..ta’limning mazmuni: o’quv rejalarida, dasturlarida va darsliklarda aks ettiriladi',
+            'o’quv rejasida va darsliklarda aks ettiriladi',
+            ' ta’limning mazmuni o’quv materiallarda aks ettiriladi',
+            ' ta’limning mazmuni darsliklarda aks ettiriladi',
+        ],
+        javob: '..ta’limning mazmuni: o’quv rejalarida, dasturlarida va darsliklarda aks ettiriladi'
+    },
+    {
+        savol: 'pedagogik tajriba tashkil etilish sharoitiga ko‘ra qanday guruhlarga ajratiladi?',
+        variantlar: ['..tabiiy tajriba, laboratoriya tajribasi, tajriba ishi',
+            ' evristik tajriba, ijodiy tajriba, innovatsion tajriba',
+            'individual tajriba, guruh tajribasi, jamoaviy tajriba',
+            ' laboratoriya tajribasi, amaliy tajriba, individual tajriba'],
+        javob: '..tabiiy tajriba, laboratoriya tajribasi, tajriba ishi'
+    },
+    {
+        savol: 'insonning biologik mohiyatini o‘zida aks ettiruvchi tushunchani aniqlang:',
+        variantlar: ['..individ',
+            'sub’ekt',
+            ' shaxs',
+            ' inson'
+        ],
+        javob: '..individ'
+    },
+    {
+        savol: 'individ kim?',
+        variantlar: ['..xatti-harakatlarni shartli reflekslar asosida tashkil etuvchi biologik mavjudot',
+            ' xatti-harakatlarni ongli boshqarish imkoniyatiga ega bo‘lmagan biologik mavjudot',
+            'xatti-harakatlarni shartsiz reflekslar asosida tashkil etuvchi biologik mavjudot',
+            'xatti-harakatlarni ongli ravishda tashkil etuvchi biologik mavjudot'
+        ],
+        javob: '..xatti-harakatlarni shartli reflekslar asosida tashkil etuvchi biologik mavjudot'
+    },
+    {
+        savol: ' shaxs kim?',
+        variantlar: ['..psixologik jihatdan taraqqiy etgan, shaxsiy xususiyatlari va xatti-harakatlari bilan boshqalardan ajralib turuvchi, muayyan xulq-atvor va dunyoqarashga ega bo‘lgan ijtimoiy mavjudot',
+            'xatti-harakatlari bilan boshqalardan ajralib turuvchi, muayyan xulq-atvor va dunyoqarashga ega bo‘lgan ijtimoiy mavjudot',
+            ' shaxsiy xususiyatlari va xatti-harakatlari bilan boshqalardan ajralib turuvchi ijtimoiy mavjudot',
+            'psixologik jihatdan taraqqiy etgan hamda ma’lum darajada ma’naviy-axloqiy sifatlarga ega ijtimoiy mavjudot'],
+        javob: '..psixologik jihatdan taraqqiy etgan, shaxsiy xususiyatlari va xatti-harakatlari bilan boshqalardan ajralib turuvchi, muayyan xulq-atvor va dunyoqarashga ega bo‘lgan ijtimoiy mavjudot'
+    },
+    {
+        savol: 'shaxsga xos bo‘lgan barcha tug‘ma xususiyatlarning boyib borishi, yangi sifatlarning hosil qilinishi, shuningdek, organizmning anatomik tuzilishi, fiziologik va psixologik funksiyalarning takomillashishi, faoliyat hamda xatti-harakatning miqdor va sifat jihatdan o‘zgarib borish jarayoni deganda nima tushuniladi?',
+        variantlar: ['..shaxsning rivojlanishi', 'shaxsning o‘sishi',
+            ' shaxsning shakllanishi', ' shaxsning takomillashuvi'],
+        javob: '..shaxsning rivojlanishi'
+    },
+
+
+    {
+        savol: 'shaxs rivojlanishiga qanday omillar ta’sir ko‘rsatadi?',
+        variantlar: ['..irsiyat, muhit, tarbiya', 'tabiiy muhit, ijtimoiy munosabatlar',
+            'muhit, tarbiya, bilim',
+            ' irsiyat, tabiiy muhit, ijtimoiy munosabatlar'],
+        javob: '..irsiyat, muhit, tarbiya'
+    },
+
+
+    {
+        savol: ' “inson kamolatida uch narsa - irsiyat, muhit, tarbiya muhim rol o‘ynaydi” degan fikrni ilk bor ilgari surgan sharq mutafakkiri kim?',
+        variantlar: ['..Abu Rayhon Beruniy',
+            'Abu Nasr Forobiy',
+            'Abu Ali Ibn Sino',
+            'Yusuf Hos Hojib'
+        ],
+        javob: '..Abu Rayhon Beruniy'
+    },
+
+
+    {
+        savol: ' shaxsning rivojlanishi qanday holatlar bilan belgilanadi?',
+        variantlar: ['..shaxsning fiziologik, ruhiy, intellektual o‘sishini ta’minlovchi faoliyat hamda xatti-harakatlarning miqdor va sifat jihatdan o‘zgarishlar bilan',
+            'shaxsning fiziologik, ruhiy kamolotga erishishida kechadigan miqdor o‘zgarish bilan', ' shaxsning fiziologik, ruhiy kamol topishida kechadigan sifat    ozgarishlar bilan',
+            'shaxs kamolotida muhim o‘rin tutuvchi faoliyat ko‘nikmalarining miqdor va sifat jihatdan takomillashib borishi bilan'],
+
+        javob: '..shaxsning fiziologik, ruhiy, intellektual o‘sishini ta’minlovchi faoliyat hamda xatti-harakatlarning miqdor va sifat jihatdan o‘zgarishlar bilan'
+    },
+
+
+    {
+        savol: 'shaxsga xos individual xususiyatlarni ko‘rsating',
+        variantlar: ['..shaxsning yo‘nalganligi, qiziqishlari, xarakteri, uni boshqalardan ajratib turadigan aqliy kamoloti, faoliyatining o‘ziga xosligini belgilovchi barqaror, muhim xususiyatlar',
+            ' shaxsning yo‘nalganligi, qiziqishlari, xarakteri, uni boshqalardan ajratib turadigan aqliy kamolotini yorituvchi barqaror, muhim xususiyatlar',
+            'shaxsning yo‘nalganligi, qiziqishlari, xarakterini tavsiflovchi barqaror, muhim xususiyatlar',
+            'shaxsning qiziqishlari, xarakteri, aqliy kamoloti, faoliyatining o‘ziga xosligini ifodalovchi barqaror, muhim xususiyatlar'],
+        javob: '..shaxsning yo‘nalganligi, qiziqishlari, xarakteri, uni boshqalardan ajratib turadigan aqliy kamoloti, faoliyatining o‘ziga xosligini belgilovchi barqaror, muhim xususiyatlar'
+    },
+
+    {
+        savol: '“qobiliyatni tarbiya qilmaslik zulmdir va qobiliyatsizga tarbiya hayf”. Mazkur fikrning muallifi kim?',
+        variantlar: ['..Alisher Navoiy',
+            'Unsurul Maoliy Kaykovus',
+            'Ahmad Yugnakiy',
+            'Saidahmad Siddiqiy'],
+        javob: '..Alisher Navoiy'
+    },
+
+]
+
+let correct_answers = 0;
+let wrong_answers = []
+let wrong_answers_answer = []
+
+function testing() {
+    let question = document.querySelector('#question');
+    let variant1 = document.querySelector('#variant1')
+    let variant2 = document.querySelector('#variant2')
+    let variant3 = document.querySelector('#variant3')
+    let variant4 = document.querySelector('#variant4')
+
+
+
+
+    let randomSon = Math.floor(Math.random(savollar.length) * 10);
+    let newMassiv = []
+
+    for (let i = 0; i <= savollar.length; i++) {
+        let element = savollar.at(randomSon - i);
+        newMassiv.push(element)
+    }
+
+
+
+
+    let count = 0;
+    question.textContent = `${count + 1}.${newMassiv[0].savol}`
+    variant1.textContent = `A) ${newMassiv[0].variantlar[0]}`
+    variant2.textContent = `B) ${newMassiv[0].variantlar[3]}`
+    variant3.textContent = `C) ${newMassiv[0].variantlar[2]}`
+    variant4.textContent = `D) ${newMassiv[0].variantlar[1]}`
+
+    function next_test() {
+        count++
+        if (count>= savollar.length) {
+            test_box.style.display = 'none';
+            show_result()
+        }
+        else {
+            let random_var = Math.floor(Math.random() * 4);
+            question.textContent = `${count + 1}.${newMassiv[count].savol}`
+
+            variant1.textContent = `A) ${newMassiv.at(count).variantlar.at(random_var - 0)}`
+            variant2.textContent = `B) ${newMassiv.at(count).variantlar.at(random_var - 1)}`
+            variant3.textContent = `C) ${newMassiv.at(count).variantlar.at(random_var - 2)}`
+            variant4.textContent = `D) ${newMassiv.at(count).variantlar.at(random_var - 3)}`
+            variants.forEach((element) => {
+                element.classList.remove('correct')
+                element.classList.remove('wrong')
+            })
+        }
+    }
+
+    // bu yerdan boshlab testni tekshiruvchi kod yoziladi
+
+    let variants = document.querySelectorAll('.variant');
+
+    variants.forEach((el) => {
+        el.addEventListener('click', (event) => {
+
+            if (el.textContent.slice(3) == newMassiv[count].javob) {
+                console.log('true')
+                el.classList.add('correct')
+                correct_answers++;
+                console.log(correct_answers)
+
+            }
+
+            else {
+                console.log('false')
+                el.classList.add('wrong')
+                wrong_answers.push(newMassiv[count].savol)
+                wrong_answers_answer.push(newMassiv[count].javob)
+                console.log(wrong_answers)
+                console.log(wrong_answers_answer)
+            }
+
+            // console.log(el.textContent.slice(3))
+            // console.log(newMassiv[count].javob)
+
+            setTimeout(next_test, 100)
+        })
+    })
+
+}
+
+let test_box = document.querySelector('.test-box');
+
+test_box.style.display = 'none';
+
+
+
+let start = document.querySelector('#start-test')
+let userName = document.querySelector('#user-name')
+let must_fill = document.querySelector('#required')
+let user_win = document.querySelector('.user')
+
+start.addEventListener('click', () => {
+    if (userName.value.length > 0) {
+        test_box.style.display = 'flex'
+        user_win.style.display = 'none'
+        testing()
+    }
+
+    else {
+        must_fill.textContent = "Ism familyani kiriting iltimos"
+    }
+
+
+})
+
+
+// user_win.style.display= 'none'
+
+
+//  bu joydan boshlab result qismining kodlari yozilgan
+
+let result = document.querySelector('.result')
+let u_name = document.querySelector('#name')
+let result_span = document.querySelector('#result')
+let wrongs = document.querySelector('#my_wrong')
+let show_hide = document.querySelector('#show-me')
+
+result.style.display='none'
+function show_result() {
+    result.style.display ='block'
+    u_name.textContent = 'Name: ' + userName.value;
+    result_span.textContent = `Natija: siz ${correct_answers}/${savollar.length} natijaga erishdingiz`
+
+    for (let i = 0; i < wrong_answers.length; i++) {
+        wrongs.innerHTML += `<p>Savol: ${wrong_answers[i]}</p>`
+        wrongs.innerHTML += `<p>Javob: ${wrong_answers_answer[i].slice(2)}</p><br><hr>
+            `
+    }
+    show_hide.addEventListener('click',()=>{
+        wrongs.classList.toggle('show-hide')
+    })
+}
+
+
